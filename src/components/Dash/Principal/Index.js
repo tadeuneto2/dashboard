70fd4e2c12efeb36925cf451e6157874bc1f6eb6
tadeu.neto <tadeu.neto@maisvida.med.br>
2019-06-04 17:36:10 -0300

    Alteração no stilo e iniciando componentes

--- src/components/Dash/Principal/Index.js
+++ src/components/Dash/Principal/Index.js
@@ -3,8 +3,22 @@ import React from 'react'
 const Principal = props => {
     return (
         <section>
-            <h1>Principal</h1>
-            <p>Aqui fica a tela principal</p>
+            <section className='resumo'>
+                <div className='min-box bg-default'>
+                    <h1>100</h1>
+                    <p><small>Usuários do sistema</small></p>
+                </div>
+                <div className='min-box bg-primary'>
+                    <h1>100</h1>
+                </div>
+                <div className='min-box bg-alter'>
+                    <h1>100</h1>
+                    <p><small>aqui fica um teste de acordo com o conteúdo dentro da caixa</small></p>
+                </div>
+            </section>
+            <section className=''>
+
+            </section>
         </section>
     )
 }
