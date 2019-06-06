869af79326dfc5f2bd5a6e292d4d60024eeeda72
tadeu.neto <tadeu.neto@maisvida.med.br>
2019-06-06 16:26:01 -0300

    criando lista de produtos melhorias

--- src/components/Dash/Produtos/index.js
+++ src/components/Dash/Produtos/index.js
@@ -1,48 +1,75 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BASE_URL } from '../../../config';
-import { async } from 'q';
 
 const ListaProdutos = (props) => {
 
     const [listProduct, setListProduct] = useState([])
+    const [listCategories, setListCategorie] = useState([])
 
     useEffect(() => {
-        const result = async () =>{
+        const resultProduct = async () => {
             const response = await axios.get(`${BASE_URL}/products`);
             setListProduct(response.data);
+            const response2 = await axios.get(`${BASE_URL}/categories`);
+            setListCategorie(response2.data);
         };
-        result();
-    });
+        resultProduct();
+    }, []);
+
+    function handleChange (event){
+        const id = event.target.value
+        console.log(id)
+    }
     return (
-        <section >
-            <table>
-                <thead>
-                    <tr>
-                        <th>Produto</th>
-                        <th>Descrição</th>
-                        <th>Categorias</th>
-                        <th>Preço</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {listProduct.map(product => (
-                        <tr key={product._id} >
-                            <td>{product.title}</td>
-                            <td>{product.description}</td>
-                            <td>
-                                <ul>
-                                    {product.categories.map((categorie, index) => {
-                                        return (<li key={index}> {categorie.title} </li>)
-                                    })}
-                                </ul>
-                            </td>
-                            <td>{product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</td>
+        <section>
+            <section className='filterTable'>
+
+                <div>
+                    <input type='text' />
+                </div>
+                <div>
+                    <p>Filtrar tabela por</p>
+                        {listCategories.map((categorie) => (
+                            <span>
+                           <input key={categorie._id} type='checkbox' value={categorie._id} onChange={handleChange} /> <label>{categorie.title}</label>
+                            </span>
+                        ))}
+                </div>
+
+
+
+            </section>
+            <section >
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Produto</th>
+                            <th>Descrição</th>
+                            <th>Categorias</th>
+                            <th>Preço</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {listProduct.map(product => (
+                            <tr key={product._id} >
+                                <td>{product.title}</td>
+                                <td>{product.description}</td>
+                                <td>
+                                    <ul>
+                                        {product.categories.map((categorie, index) => {
+                                            return (<li key={index}> {categorie.title} </li>)
+                                        })}
+                                    </ul>
+                                </td>
+                                <td>{product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </section>
         </section>
+
     )
 
 }
